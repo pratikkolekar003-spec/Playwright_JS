13006c1e5daf87a86676ea12a3d59678df4ce408
Pratik Kolekar <pratikkolekar003@example.com>
2026-09-23 06:46:59 +0530

    Add Playwright test cases

---
+++ tests/Basic.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test ('first test case', async ({page})=>{
+
+    await page.goto('https://www.automationpracticehub.com/');
+    await expect(page).toHaveURL('https://www.automationpracticehub.com/');
+
+    const userName = await page.locator('#username');
+    const passWord = await page.locator('#password');
+
+    await userName.fill('sagesyntaxacademy');
+    await passWord.fill('BuildingExcellence@111');
+
+    await expect(userName).toHaveValue('sagesyntaxacademy');
+    await expect(passWord).toHaveValue('BuildingExcellence@111');
+
+    await page.getByRole('checkbox', {name : 'I Agree to the'}).check();
+    await page.getByRole('button', {name : 'Sign In'}).click();
+
+    await page.pause()
+})
